d02765e7fe9f30df6670bc829bd6de7cf320b61a
yshrsmz <the.phantom.bane@gmail.com>
2013-09-09 17:23:03 +0900

    add tmux-powerline settings

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ ln -s ${HOME}/doftiles/_tmuxinator ${HOME}/.tmuxinator
 ln -s ${HOME}/dotfiles/_zshrc ${HOME}/.zshrc
 ln -s ${HOME}/dotfiles/_vimrc ${HOME}/.vimrc
 ln -s ${HOME}/dotfiles/_tmux.conf ${HOME}/.tmux.conf
+ln -s ${HOME}/dotfiles/_tmux-powerlinerc ${HOME}/.tmux-powerlinerc
 
 # set up git submodules
 cd ${HOME}/dotfiles
