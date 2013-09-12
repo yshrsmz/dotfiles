ed1eccc64ce49d30c279ba8a653fc0302285513b
yshrsmz <the.phantom.bane@gmail.com>
2013-09-12 18:51:05 +0900

    add Homebrew package

--- Homebrew.sh
+++ Homebrew.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+brew install git
+brew install rbenv
+brew install ruby-build
+brew install readline
+brew install openssl
 brew install zsh
 brew install z
+brew install tree
 brew install tmux
 brew install reattach-to-user-namespace
